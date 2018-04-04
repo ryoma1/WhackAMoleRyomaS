fb2c7305f4cf6197727091258c27043d6fcb3de6
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-04-04 11:44:56 -0400

    yes

--- main.lua
+++ main.lua
@@ -1,7 +1,18 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: WhackAMole
+-- Name: Ryoma Scott
+-- Course: ICS2O/3C
+-- This program places a random object on the screen. If the user clicks on it it in time,
+-- the score increases by 1.
 
--- Your code here
+--hide the status bar.
+display.setStatusBar(display.HiddenStatusBar)
+
+Creating Background
+local bkg = display.newRect( 0, 0, display.contentWidth, display.contentHeight )
+
+--set the background colour
+display.setDefault("background", 100/255, 255/255, 100/255)
+
+------------------------------------------------------------------
+--LOCAL VARIABLES
+------------------------------------------------------------------
